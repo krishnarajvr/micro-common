69e0ed419ee1076446462dfa2f350fb92e212f3b
krishnaraj <krishnaraj.vr@impelsys.com>
2021-02-01 09:51:22 +0530

    Support validation messages

--- validation.go
+++ validation.go
@@ -6,6 +6,33 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// GetMessages - Get the message based on the language
+func GetMessages(langKey string) map[string]string {
+	return map[string]string{
+		"Required":     "Can not be empty",
+		"Min":          "Minimum is %d",
+		"Max":          "Maximum is %d",
+		"Range":        "Range is %d to %d",
+		"MinSize":      "Minimum size is %d",
+		"MaxSize":      "Maximum size is %d",
+		"Length":       "Required length is %d",
+		"Alpha":        "Must be valid alpha characters",
+		"Numeric":      "Must be valid numeric characters",
+		"AlphaNumeric": "Must be valid alpha or numeric characters",
+		"Match":        "Must match %s",
+		"NoMatch":      "Must not match %s",
+		"AlphaDash":    "Must be valid alpha or numeric or dash(-_) characters",
+		"Email":        "Must be a valid email address",
+		"IP":           "Must be a valid ip address",
+		"Base64":       "Must be valid base64 characters",
+		"Mobile":       "Must be valid mobile number",
+		"Tel":          "Must be valid telephone number",
+		"Phone":        "Must be valid telephone or mobile phone number",
+		"ZipCode":      "Must be valid zipcode",
+	}
+}
+
+// ValidateForm - Validate the data from request based on the form definition
 func ValidateForm(c *gin.Context, form interface{}) (status bool, errorData *ErrorData) {
 	err := c.Bind(form)
 	if err != nil {
@@ -14,6 +41,7 @@ func ValidateForm(c *gin.Context, form interface{}) (status bool, errorData *Err
 
 	log := c.MustGet("log").(*logrus.Logger)
 
+	validation.MessageTmpls = GetMessages("en")
 	valid := validation.Validation{}
 	check, err := valid.Valid(form)
 
